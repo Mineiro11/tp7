5c4db5e043bd7dfb491dfd3a4f41e1fdd92307f9
Android Studio <devandroid@ort>
2025-05-19 12:04:14 -0300

    avanzado

--- App.js
+++ App.js
@@ -1,44 +1,5 @@
-/*import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <ImageBackground source={require("./assets/fortmapa.jpg")} resizeMode="cover" style={styles.background}>
-      <Image source={require("./assets/tralalilouuu.jpg")}  resizeMode="cover" style={styles.Image}></Image>
-      <Text>milo</Text>
-      <StatusBar style="auto" />
-      </ImageBackground>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    width: '100',
-    height: '100',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  Image: {
-    width: '100',
-    height: '100',
-   },
-    container: {
-      width: 100,
-      height: 100,
-      borderRadius: 50, 
-      overflow: 'hidden',
-  }
-})*/
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
-
+import { StyleSheet, Text, View, ImageBackground, Image, TextInput,TouchableOpacity } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
@@ -47,19 +8,28 @@ export default function App() {
         resizeMode="cover"
         style={styles.background}
       >
-        <View style={styles.perfilContainer}>
-          <Image
-            source={require("./assets/tralalilouuu.jpg")}
-            resizeMode="cover"
-            style={styles.perfil}
+        {}
+        <View style={styles.card}>
+          <View style={styles.perfilContainer}>
+            <Image
+              source={require("./assets/tralalilouuu.jpg")}
+              resizeMode="cover"
+              style={styles.perfil}
+            />
+          </View>
+          <Text style={styles.text}>Milo y Lewko</Text>
+          <Text style={styles.text}>Desarrolladores de Frontend con experiencia.</Text>
+          <TextInput style={styles.textInput}placeholder="Escribe un mensaje"
           />
+         <TouchableOpacity style={styles.boton}><Text style={styles.botonTexto}>Contactar</Text></TouchableOpacity>
+
         </View>
-        <Text style={styles.text}>Milo</Text>
         <StatusBar style="auto" />
       </ImageBackground>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -75,20 +45,56 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  card: {
+    backgroundColor: '#fff', 
+    padding: 20,
+    borderRadius: 20,
+    alignItems: 'center',
+    elevation: 5, 
+  },
+  textInput: {
+    height: 40,
+    width: 250,
+    borderColor: '#1a1110',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginTop: 10,
+  },
+  boton: {
+    height: 40,
+    width: 250,
+    backgroundColor: '#008000',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  
+  botonTexto: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  
+  
   perfilContainer: {
     width: 100,
     height: 100,
     borderRadius: 50,
+    borderColor: '#1a1110',
+    borderWidth: 1,
     overflow: 'hidden',
     marginBottom: 20,
   },
   perfil: {
     width: '100%',
     height: '100%',
+    
   },
   text: {
     fontSize: 24,
-    color: '#fff',
+    color: '#1a1110',
     fontWeight: 'bold',
   },
 });
